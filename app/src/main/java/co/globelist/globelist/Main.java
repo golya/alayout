65811d271cf266364b56f02434f89d57ca29ab67
Ádám Gólya <adam.stork@gmail.com>
2015-12-30 16:34:12 +0100

    refactor(Main.java): reorganize methods and remove google API
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app/src/main/java/co/globelist/globelist/Main.java
+++ app/src/main/java/co/globelist/globelist/Main.java
@@ -3,54 +3,60 @@ package co.globelist.globelist;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Message;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 
 import com.daililol.material.appbase.base.BaseNavigationDrawerActivity;
 import com.daililol.material.appbase.component.BaseNavigationDrawerListAdapter;
+import com.daililol.material.appbase.utility.Converter;
 import com.daililol.material.appbase.utility.DrawableUtil;
 import com.daililol.material.appbase.widget.ExtendableListView;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Main extends BaseNavigationDrawerActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
     @Override
-    protected void onActivityCreated(Bundle savedInstanceState) {
-        // TODO this method will be involved when all start-up tasks are done.
-        // Do your own work here
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
-    protected int setupContentVew() {
-        // TODO return your main content view layout
-        return R.layout.actionbar;
+    protected void setupNavigationDrawerItem(ExtendableListView listView, BaseNavigationDrawerListAdapter navigationListAdapter) {
+        listView.setBackgroundColor(Color.WHITE);
+        navigationListAdapter.addItem("item 1",
+                DrawableUtil.getDrawable(this, R.drawable.ic_menu_black_24dp));
+        navigationListAdapter.addItem("item 2",
+                DrawableUtil.getDrawable(this, R.drawable.ic_close_grey600_18dp));
+    }
+
+    @Override
+    protected View setupNavigationDrawerHeader() {
+        AbsListView.LayoutParams params;
+        params = new AbsListView.LayoutParams(AdapterView.LayoutParams.MATCH_PARENT, Converter.dp2px(this, 20));
+        LinearLayout header = new LinearLayout(this);
+        header.setLayoutParams(params);
+        header.setBackgroundColor(this.getResources().getColor(com.daililol.material.appbase.R.color.base_theme_blue));
+        return header;
     }
 
     @Override
     protected Drawable setupThemeColor() {
-        // TODO return your actionbar background color (or image) here
         return new ColorDrawable(Color.YELLOW);
     }
 
     @Override
+    protected int setupContentVew() {
+        return R.layout.actionbar;
+    }
+
+    @Override
     protected void onMenuItemSelected(MenuItem menu) {
-        // TODO this method will be involved when an menu item is selected or clicked
-        Log.d("Main", menu.toString());
         switch (menu.getItemId()) {
             case android.R.id.home:
                 //finish();
@@ -61,16 +67,7 @@ public class Main extends BaseNavigationDrawerActivity {
     }
 
     @Override
-    protected void setupNavigationDrawerItem(ExtendableListView listView, BaseNavigationDrawerListAdapter navigationListAdapter) {
-        navigationListAdapter.addItem("item 1",
-                DrawableUtil.getDrawable(this, R.drawable.ic_menu_black_24dp));
-        navigationListAdapter.addItem("item 2",
-                DrawableUtil.getDrawable(this, R.drawable.ic_close_grey600_18dp));
-    }
-
-    @Override
-    protected View setupNavigationDrawerHeader() {
-        return null;
+    protected void onActivityCreated(Bundle savedInstanceState) {
     }
 
     @Override
@@ -111,54 +108,5 @@ public class Main extends BaseNavigationDrawerActivity {
     @Override
     public void onDrawerStateChanged(int newState) {
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://co.globelist.globelist/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://co.globelist.globelist/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
